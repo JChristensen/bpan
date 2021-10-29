a6767123bd841e74885f5f6219c7e6155155f79d
JChristensen <jack.christensen@outlook.com>
2021-10-29 12:59:09 -0400

    Enable lingering so the systemd user manager runs even when the user is not logged on.

--- install-unit-files.sh
+++ install-unit-files.sh
@@ -4,4 +4,5 @@
 
 cp -av *.service *.timer /home/$USER/.config/systemd/user/
 sed -i s/UUUU/$USER/ /home/$USER/.config/systemd/user/bpan.service
+loginctl enable-linger $USER
 systemctl --user daemon-reload
