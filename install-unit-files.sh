93e215bb16f585f0f26f25c15eca5d07f0c90ece
JChristensen <jack.christensen@outlook.com>
2021-10-30 21:36:35 -0400

    Ensure systemd user directory exists.

--- install-unit-files.sh
+++ install-unit-files.sh
@@ -2,7 +2,13 @@
 # Copy systemd unit files (.service, .timer) to
 # /home/$USER/.config/systemd/user/
 
-cp -av *.service *.timer /home/$USER/.config/systemd/user/
-sed -i s/UUUU/$USER/ /home/$USER/.config/systemd/user/bpan.service
+# ensure the systemd user directory exists
+sysdUser="/home/$USER/.config/systemd/user"
+if [ ! -d $sysdUser ]; then
+    mkdir -p $sysdUser
+fi
+
+cp -av *.service *.timer $sysdUser
+sed -i s/UUUU/$USER/ $sysdUser/bpan.service
 loginctl enable-linger $USER
 systemctl --user daemon-reload
